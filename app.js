212e0c6cddaf956a240817ef7743c063da6dcc89
Harshtandel95 <harsh@markweb.in>
2025-03-04 21:13:15 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT ;
 const MONGODB_URI = process.env.MONGODB_URI ;
 // Middleware
 app.use(cors({
-  origin: 'https://article-composer-frontend.vercel.app/',  // Allow all origins
+  origin: '*',  // Allow all origins
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
